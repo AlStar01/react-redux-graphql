5d88d38df42a264c50710dca71f4eb42dcfdba74
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-02 17:54:46 -0600

    Added About page link in Home page component

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -10,6 +10,10 @@ export default class About extends Component {
                 <Col sm={12}>
                     <h1>About</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum rerum, explicabo omnis eaque nam adipisci ratione magni, inventore deserunt mollitia consequatur quam sit officia deleniti? Minus porro, expedita eveniet fugit?</p>
+
+                    <br/>
+
+                    <Link to="about" className="btn btn-primary">Learn more</Link>
                 </Col>
             </Row>
         );
