fcbc48fab1be86c8959bb15390713719b8a52c56
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-02 17:24:43 -0600

    Created basic Home page component

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>React Redux GraphQL</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate incidunt, excepturi pariatur corporis quam voluptas reiciendis. Ipsam eligendi beatae, eos soluta enim earum aspernatur, officiis recusandae! Sapiente illum fuga quis!</p>
+            </div>
+        );
+    }
+}
