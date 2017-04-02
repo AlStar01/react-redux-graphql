93fe3eab7c5be15b79bbc6b4579ac0b147746bef
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-02 17:53:53 -0600

    Created basic About page component

---
+++ client/src/components/about/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+import { Row, Col } from 'react-bootstrap';
+
+export default class About extends Component {
+    render() {
+        return (
+            <Row>
+                <Col sm={12}>
+                    <h1>About</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum rerum, explicabo omnis eaque nam adipisci ratione magni, inventore deserunt mollitia consequatur quam sit officia deleniti? Minus porro, expedita eveniet fugit?</p>
+                </Col>
+            </Row>
+        );
+    }
+}
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import { Row, Col } from 'react-bootstrap';
+
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
-                <h1>React Redux GraphQL</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate incidunt, excepturi pariatur corporis quam voluptas reiciendis. Ipsam eligendi beatae, eos soluta enim earum aspernatur, officiis recusandae! Sapiente illum fuga quis!</p>
-            </div>
+            <Row>
+                <Col sm={12}>
+                    <h1>React Redux GraphQL</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate incidunt, excepturi pariatur corporis quam voluptas reiciendis. Ipsam eligendi beatae, eos soluta enim earum aspernatur, officiis recusandae! Sapiente illum fuga quis!</p>
+                </Col>    
+            </Row>
         );
     }
 }
