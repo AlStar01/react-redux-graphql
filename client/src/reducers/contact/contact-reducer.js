2ac0b432fd2d48e5b0c5504ac0daa45b9a4ad7b3
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-21 14:50:03 -0600

    Created file with export object to represent initial/default state. Updated contacts reducer to utilize this.

--- client/src/reducers/contact/contact-reducer.js
+++ client/src/reducers/contact/contact-reducer.js
@@ -1,6 +1,7 @@
 import * as types from '../../actions/contact/contact-action-types';
+import initialState from '../initialState';
 
-export default function contactReducer(state = [], action) {
+export default function contactReducer(state = initialState.contacts, action) {
     switch(action.type) {
         case types.ADD_CONTACT:
             return [...state, Object.assign({}, action.contact)];
