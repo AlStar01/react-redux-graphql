9db690e9cf46a0a47ace03e92a1d7d6e9203c383
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-22 15:38:03 -0600

    Created const type for create and update contact. Added thunk for saving contact (create or update depending on situation).

--- client/src/actions/contact/contact-actions.js
+++ client/src/actions/contact/contact-actions.js
@@ -10,11 +10,30 @@ export function loadContactsSuccess(contacts) {
     return { type: types.LOAD_CONTACTS_SUCCESS, contacts };
 }
 
+export function createContactSuccess(contact) {
+    return { type: types.CREATE_CONTACT_SUCCESS, contact };
+}
+
+export function updateContactSuccess(contact) {
+    return { type: types.UPDATE_CONTACT_SUCCESS, contact };
+}
+
 export function loadContacts(dispatch) {
     return function(dispatch) {
         return ContactService.getContacts()
             .then(response => response.json())
             .then(contacts => dispatch(loadContactsSuccess(contacts)))
+            .catch(err => console.error(err));
+    }
+}
+
+export function saveContact(contact) {
+    return function (dispatch) {
+        return ContactService.saveContact(contact)
+            .then(response => response.json())
+            .then(savedContact => {
+                contact.id ? dispatch(updateContactSuccess(savedContact)) : dispatch(createContactSuccess(savedContact)));
+            })
             .catch(err => console.error(err));
     }
 }
